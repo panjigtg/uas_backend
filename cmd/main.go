df5cfce317f15b4f78957a4bcb4e04cba78069e1
Panjiiw <panjiiwbesarik114@gmail.com>
2025-12-16 22:39:14 +0700

    FR-011 & add Swagger

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,19 @@ import (
 	"uas/config"
 	"path/filepath"
 	"log"
+	_ "uas/cmd/docs"
 
 	"github.com/joho/godotenv"
+	"github.com/gofiber/swagger"
 )
 
+// @title           UAS Achievement API
+// @version         1.0
+// @description     API Sistem Prestasi Mahasiswa
+// @BasePath        /api/v1
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 func main() {
 	envPath, _ := filepath.Abs(".env")
 
@@ -16,6 +25,6 @@ func main() {
 	}
 
 	app := config.Bootstrap()
-	
+	app.Get("/swagger/*", swagger.HandlerDefault)
 	app.Listen(":3000")
 }
